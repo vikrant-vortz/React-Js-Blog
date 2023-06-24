181c9f853d3990138288f6f11dca1784807fb912
Vikrant-vortz <n0w0rri3s7@gmail.com>
2023-06-24 13:16:56 +0530

    Custom Hooks Lesson 21 complete

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
 import api from './api/posts';
 import useWindowSize from './hooks/useWindowSize';
+import useAxiosFetch from './hooks/useAxiosFetch';
+
 
 function App() {
   const [posts, setPosts] = useState([])
@@ -23,26 +25,11 @@ function App() {
   const [editBody, setEditBody] = useState('');
   const history = useHistory();
   const { width } = useWindowSize();
+  const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts');
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await api.get('/posts');
-        setPosts(response.data);
-      } catch (err) {
-        if (err.response) {
-          // Not in the 200 response range 
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.headers);
-        } else {
-          console.log(`Error: ${err.message}`);
-        }
-      }
-    }
-
-    fetchPosts();
-  }, [])
+    setPosts(data);
+  }, [data])
 
   useEffect(() => {
     const filteredResults = posts.filter((post) =>
@@ -100,7 +87,11 @@ function App() {
       <Nav search={search} setSearch={setSearch} />
       <Switch>
         <Route exact path="/">
-          <Home posts={searchResults} />
+          <Home 
+          fetchError={fetchError}
+          isLoading={isLoading}
+          posts={searchResults}
+           />
         </Route>
         <Route exact path="/post">
           <NewPost
