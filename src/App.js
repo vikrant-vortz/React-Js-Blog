2c2825aff5cb08bdbea2b2ca3dad19ce6708f451
Vikrant-vortz <n0w0rri3s7@gmail.com>
2023-06-24 12:16:13 +0530

    All file added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { Route, Switch, useHistory } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
 import api from './api/posts';
+import useWindowSize from './hooks/useWindowSize';
 
 function App() {
   const [posts, setPosts] = useState([])
@@ -21,6 +22,7 @@ function App() {
   const [editTitle, setEditTitle] = useState('');
   const [editBody, setEditBody] = useState('');
   const history = useHistory();
+  const { width } = useWindowSize();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -94,7 +96,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header title="React JS Blog" />
+      <Header title="React JS Blog" width={width}/>
       <Nav search={search} setSearch={setSearch} />
       <Switch>
         <Route exact path="/">
